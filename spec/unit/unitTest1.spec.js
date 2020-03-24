b0932660cc9af8342fbcbd352d92b40467e38313
Ilham Gibran <ilgibrz@gmail.com>
2020-03-24 20:41:33 +0700

    Add Test Suite for Test Sequence Structure

--- spec/unit/unitTest1.spec.js
+++ spec/unit/unitTest1.spec.js
@@ -2,15 +2,39 @@ var sequence = require('../../struct/sequence');
 
 describe("UNIT TEST NO 1 : Standard Sequence ", function() {
     // Variable Global bisa dipake fungsi manapun
-    var _numberA;
+    var _numberA, _numberB;
 
     beforeEach(function() {
     });
 
-    it("Harus menampilkan x + 7", function() {
+    it("Harus menampilkan 6 + 7 = 13", function() {
         _numberA = 6;
         var result = sequence.seq1Tambah7(_numberA);
 
         expect(result).toEqual(13);
+    });
+
+    it("Harus menampilkan (2*16) + 9 + 7 = 48", function(){
+        _numberA = 4;
+        _numberB = 3;
+        var result = sequence.seq2SPL(_numberA, _numberB);
+
+        expect(result).toEqual(48);
+    });
+
+    it("Menampilkan nilai 2 > 5 = False", function(){
+        _numberA = 2;
+        _numberB = 5;
+        var result = sequence.seq3Relational(_numberA, _numberB);
+
+        expect(result).toBeFalse();
+    });
+
+    it("Menampilkan nilai !(3 < 5 && 4 > 2) = False", function(){
+        _numberA = 3;
+        _numberB = 4;
+        var result = sequence.seq4Boolean(_numberA, _numberB);
+
+        expect(result).toBeFalse();
     });
 });
--- struct/sequence.js
+++ struct/sequence.js
@@ -3,6 +3,22 @@ function sequence() {
     this.seq1Tambah7 = function (x) {
         return x+7;
     }
+
+    this.seq2SPL = function (x, y) {
+        var result = (2*Math.pow(x,2));
+        result += (3*y);
+        result += 7;
+
+        return result;
+    }
+
+    this.seq3Relational = function (x, y){
+        return x > y;
+    }
+
+    this.seq4Boolean = function (x, y){
+        return !(x < 5 && y > 2);
+    }
 }
 
 module.exports = new sequence();
